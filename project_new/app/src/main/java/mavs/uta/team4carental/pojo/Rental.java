bebcd61386ef1a39491a628cbf2e6137a5d16f54
Astroleander <astroleander@gmail.com>
2020-09-29 23:53:42 +0800

    Created inherits

---
+++ project_new/app/src/main/java/mavs/uta/team4carental/pojo/Rental.java
@@ -0,0 +1,7 @@
+package mavs.uta.team4carental.pojo;
+
+public class Rental extends User {
+    public Rental(String username, String password, String role, String firstname, String lastname, String phone, String email, String address, String city, String state, String zipcode, String member, String status) {
+        super(username, password, role, firstname, lastname, phone, email, address, city, state, zipcode, member, status);
+    }
+}
